ef71495c6ac5ce385a59f060c15fba93308aa2fd
bsucker98 <bmorashad@gmail.com>
2020-12-17 10:44:27 +0530

    removed deprecated addMatch method from league manager

--- app/domain/LeagueManager.java
+++ app/domain/LeagueManager.java
@@ -10,7 +10,6 @@ import domain.custom.exception.SeasonMismatchException;
 public interface LeagueManager {
 	boolean addFootballClub(FootballClub club) throws NoMoreClubsAllowed;
 	FootballClub removeFootballClub(String clubName);
-	void addMatch(Match match) throws NoSuchClubException;
 	Match addMatch(String teamA, String teamB, int teamAGoals, int teamBGoals, LocalDate date) throws 
 		NoSuchClubException, SeasonMismatchException;
 	List<FootballClub> getAllClubs();
