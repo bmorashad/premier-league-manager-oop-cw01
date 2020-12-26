16fec96698727e47391f65b06c1662ce476b6228
bsucker98 <bmorashad@gmail.com>
2020-12-26 12:36:51 +0530

    comparator imporved code

--- app/domain/model/GoalsComparator.java
+++ app/domain/model/GoalsComparator.java
@@ -4,12 +4,6 @@ import java.util.Comparator;
 public class GoalsComparator implements Comparator<FootballClub> {
 	@Override
 	public int compare(FootballClub club1, FootballClub club2) {
-		if(club1.getGoalsScored() > club2.getGoalsScored()) {
-			return 1;
-		} else if(club1.getGoalsScored() < club2.getGoalsScored()) {
-			return -1;
-		} else {
-			return 0;
-		}
+		return (club1.getGoalsScored() - club2.getGoalsScored());
 	}
 }
--- app/domain/model/PointsComparator.java
+++ app/domain/model/PointsComparator.java
@@ -4,12 +4,6 @@ import java.util.Comparator;
 public class PointsComparator implements Comparator<FootballClub> {
 	@Override
 	public int compare(FootballClub club1, FootballClub club2) {
-		if(club1.getPoints() > club2.getPoints()) {
-			return 1;
-		} else if(club1.getPoints() < club2.getPoints()) {
-			return -1;
-		} else {
-			return 0;
-		}
+		return (club1.getPoints() - club2.getPoints());
 	}
 }
