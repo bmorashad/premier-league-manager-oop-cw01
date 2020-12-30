ddd0891cddc141e6de0b08ea4dcc95724eb43184
bsucker98 <bmorashad@gmail.com>
2020-12-30 15:10:45 +0530

    path configuration bug fix

--- app/conf/PathConfiguration.java
+++ app/conf/PathConfiguration.java
@@ -3,7 +3,7 @@ package conf;
 import java.util.Arrays;
 
 public class PathConfiguration {
-	public static final String DEV_FOLDER = "app"; 
+	public static final String PROJECT_NAME = "premier-league-manager-oop-cw01"; 
 	public static final String PROJECT_ROOT; 
 	public static final String DATA_PATH;
 	public static final String UPDATES_PATH; 
@@ -17,8 +17,8 @@ public class PathConfiguration {
 		root = root.replace("\\\\", pathSeparator);
 		root = root.replace("\\", pathSeparator);
 		String[] folders = root.split(pathSeparator);
-		int indexOfProjectRootFolder = Arrays.asList(folders).lastIndexOf(DEV_FOLDER);
-		PROJECT_ROOT = String.join(pathSeparator, Arrays.copyOfRange(folders, 0, indexOfProjectRootFolder)) + pathSeparator;
+		int indexOfProjectRootFolder = Arrays.asList(folders).lastIndexOf(PROJECT_NAME);
+		PROJECT_ROOT = String.join(pathSeparator, Arrays.copyOfRange(folders, 0, indexOfProjectRootFolder + 1)) + pathSeparator;
 		DATA_PATH = PROJECT_ROOT + ".data/";
 		UPDATES_PATH = DATA_PATH + ".updates/"; 
 		CLI_UPDATES_PATH = UPDATES_PATH + "cli-updates.txt"; 
