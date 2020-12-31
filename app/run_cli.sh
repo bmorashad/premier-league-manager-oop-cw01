b69b011fc9849f72e01f35fbb8b02ac914533623
bsucker98 <bmorashad@gmail.com>
2020-12-31 15:43:26 +0530

    run_cli script improved

--- app/run_cli.sh
+++ app/run_cli.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-rm -f dao/*.class
-rm -f cli/*.class
-rm -f conf/*.class
-rm -f utils/*.class
-rm -f domain/*.class
-rm -f domain/model/*.class
-rm -f cli/custom/exception/*.class
-rm -f domain/custom/exception/*.class
+rm -rf out/*
 javac -d out/ cli/StartLeagueManagerMenu.java
 java -cp out/ cli/StartLeagueManagerMenu
