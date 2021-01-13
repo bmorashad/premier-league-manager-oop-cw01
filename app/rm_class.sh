e8dfefdef76f626e26bacff6a4776c2efea599e1
bsucker98 <bmorashad@gmail.com>
2021-01-13 15:57:28 +0530

    added script to remove all classes

---
+++ app/rm_class.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -rf domain/model/*.class
+rm -rf domain/custom/exception/*.class
+rm -rf domain/*.class
+rm -rf utils/*.class
+rm -rf dao/*.class
+rm -rf cli/custom/exception/*.class
+rm -rf cli/*.class
+rm -rf conf/*class
+rm -rf dto/*class
