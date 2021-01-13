acbc77a7040fd2f56a698b6ff42070efefbf9d1b
bsucker98 <bmorashad@gmail.com>
2021-01-13 15:35:59 +0530

    improved run script

--- app/run_cli.sh
+++ app/run_cli.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-rm -rf domain/model/*.class
-rm -rf domain/custom/exception/*.class
-rm -rf domain/*.class
-rm -rf utils/*.class
-rm -rf dao/*.class
-rm -rf cli/custom/exception/*.class
-rm -rf cli/*.class
-rm -rf conf/*class
-rm -rf dto/*class
-javac cli/StartLeagueManagerMenu.java
-java cli/StartLeagueManagerMenu
+javac -sourcepath ./ -d out -classpath out cli/StartLeagueManagerMenu.java
+env CLASSPATH=out java cli/StartLeagueManagerMenu.java
+
+# rm -rf domain/model/*.class
+# rm -rf domain/custom/exception/*.class
+# rm -rf domain/*.class
+# rm -rf utils/*.class
+# rm -rf dao/*.class
+# rm -rf cli/custom/exception/*.class
+# rm -rf cli/*.class
+# rm -rf conf/*class
+# rm -rf dto/*class
+# javac cli/StartLeagueManagerMenu.java
+# java cli/StartLeagueManagerMenu
